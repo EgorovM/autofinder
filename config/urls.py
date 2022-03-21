208f04d631af974ba1e1c48ce7722903efb67e34
EgorovM <egorov_michil@mail.ru>
2022-03-21 20:41:11 +0300

    endpoints to api

--- config/urls.py
+++ config/urls.py
@@ -36,5 +36,5 @@ urlpatterns = [
     path('api-token-auth/', auth_views.obtain_auth_token),
     path('djrichtextfield/', include('djrichtextfield.urls')),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('', include('main.urls')),
+    path('api/', include('main.urls')),
 ]
