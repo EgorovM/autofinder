937dd0448f8d9a84c5272d26c1b31a8d9931af4a
EgorovM <egorov_michil@mail.ru>
2022-03-22 22:05:04 +0300

    token auth

--- main/views.py
+++ main/views.py
@@ -1,6 +1,3 @@
-from django.views.decorators.csrf import csrf_exempt
-from django.utils.decorators import method_decorator
-
 from rest_framework import viewsets, mixins
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
@@ -16,7 +13,6 @@ from .serializers import (
 from .pagination import WorkExamplesPagination
 
 
-@method_decorator(csrf_exempt, name='dispatch')
 class BlogArticleViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = BlogArticle.objects.all().order_by('-created')
 
@@ -27,7 +23,6 @@ class BlogArticleViewSet(viewsets.ReadOnlyModelViewSet):
         return BlogDetailSerializer
 
 
-@method_decorator(csrf_exempt, name='dispatch')
 class FeedbackViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
     queryset = FeedbackContact.objects.all()
     serializer_class = FeedbackSerializer
@@ -35,7 +30,6 @@ class FeedbackViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
     permission_classes = [IsAuthenticated]
 
 
-@method_decorator(csrf_exempt, name='dispatch')
 class WorkExampleViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = WorkExample.objects.all().order_by('-created')
     pagination_class = WorkExamplesPagination
