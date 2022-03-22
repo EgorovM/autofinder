16fe7dd5e2c8ab170c2f5a19f30113af8fe918b5
EgorovM <egorov_michil@mail.ru>
2022-03-22 20:59:31 +0300

    sqlite to db

--- config/settings.py
+++ config/settings.py
@@ -136,14 +136,21 @@ DJRICHTEXTFIELD_CONFIG = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config['postgres']['name'],
-        'USER': config['postgres']['user'],
-        'PASSWORD': config['postgres']['password'],
-        'HOST': 'db',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3'
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': config['postgres']['name'],
+#         'USER': config['postgres']['user'],
+#         'PASSWORD': config['postgres']['password'],
+#         'HOST': 'db',
+#         'PORT': 5432,
+#     }
+# }
 
 
 # Password validation
